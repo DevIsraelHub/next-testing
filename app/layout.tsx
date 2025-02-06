68ebd15ed8649a55d9469086c8630d3dd7e04c5e
Dev Israel <devisrael01@gmail.com>
2025-02-06 20:11:48 +0100

    added toast

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
 import Script from "next/script";
+import { Toaster } from "@/components/ui/toaster";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -29,6 +30,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <Toaster />
         {children}
 
         {/* <script src="https://v0-next-testing-pwaxyo.vercel.app/embed.js" data-embed-id="default-embed-id"></script> */}
